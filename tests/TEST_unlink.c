8487544b30d2205ac2151283b39f31ccac35f547
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2022-12-20 17:02:52 +0000

    added tests for unlink

--- tests/TEST_unlink.c
+++ tests/TEST_unlink.c
@@ -7,11 +7,61 @@
 #include <string.h>
 #include <unistd.h>
 
+const char FILE_NAME[] = "/f1";
+const char HARD_LINK[] = "/hard";
+const char SYM_LINK[] = "/sym";
+const char FILE_CONTENT[] = "Hello padawan!";
+
+void verify_content(const char file[]) {
+    int f = tfs_open(file, TFS_O_CREAT);
+    assert(f != -1);
+
+    char buffer[sizeof(FILE_CONTENT)];
+
+    assert(tfs_read(f, buffer, sizeof(FILE_CONTENT)) == sizeof(FILE_CONTENT));
+    assert(memcmp(buffer, FILE_CONTENT, sizeof(FILE_CONTENT)) == 0);
+
+    assert(tfs_close(f) == 0);
+}
+
+void verify_file_inexistent(const char file[]) {
+    int f = tfs_open(file, TFS_O_APPEND);
+    assert(f == -1);
+}
+
 // This test asserts that multiple threads
 int main() {
     assert(tfs_init(NULL) != -1);
 
-    // TODO: tests here
+    // creates the file
+    int fhandle = tfs_open(FILE_NAME, TFS_O_CREAT);
+    assert(fhandle != -1);
+
+    assert(tfs_write(fhandle, FILE_CONTENT, sizeof(FILE_CONTENT)) ==
+           sizeof(FILE_CONTENT));
+
+    assert(tfs_close(fhandle) == 0);
+
+    assert(tfs_link(FILE_NAME, HARD_LINK) == 0);
+    assert(tfs_sym_link(FILE_NAME, SYM_LINK) == 0);
+
+    // verifies that the content is in all files
+    verify_content(FILE_NAME);
+    verify_content(HARD_LINK);
+    verify_content(SYM_LINK);
+
+    // unlink all cases and verifies that that it can be opened anymore
+    assert(tfs_unlink(FILE_NAME) == 0);
+    verify_file_inexistent(FILE_NAME);
+
+    assert(tfs_unlink(HARD_LINK) == 0);
+    verify_file_inexistent(HARD_LINK);
+
+    // all hard links were deleted, so the file must not exist, and the sym link
+    // should point to an inexistent file
+    verify_file_inexistent(SYM_LINK);
+
+    assert(tfs_unlink(SYM_LINK) == 0);
 
     // destroy TécnicoFS
     assert(tfs_destroy() != -1);
