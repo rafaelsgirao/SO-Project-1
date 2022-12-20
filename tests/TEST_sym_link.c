17a3f62241022ec785c6c242396c62e4fd418019
Guilherme Marcondes <guilherme.marcondes@tecnico.ulisboa.pt>
2022-12-20 16:43:58 +0000

    verifies circular link case

--- tests/TEST_sym_link.c
+++ tests/TEST_sym_link.c
@@ -7,11 +7,74 @@
 #include <string.h>
 #include <unistd.h>
 
+const char FILE_NAME[] = "/file";
+const char FILE_CONTENT[] = "Hello!";
+const char SYM_LINK_1[] = "/sym_1";
+const char SYM_LINK_2[] = "/sym_2";
+
 // This test asserts that multiple threads
 int main() {
     assert(tfs_init(NULL) != -1);
 
-    // TODO: tests here
+    // creates the file
+    int fhandle = tfs_open(FILE_NAME, TFS_O_CREAT);
+    assert(fhandle != -1);
+
+    // writes the content in the file
+    assert(tfs_write(fhandle, FILE_CONTENT, sizeof(FILE_CONTENT)) ==
+           sizeof(FILE_CONTENT));
+
+    // closes the file
+    assert(tfs_close(fhandle) == 0);
+    fhandle = tfs_open(FILE_NAME, TFS_O_CREAT);
+    assert(fhandle != -1);
+
+    char buffer[sizeof(FILE_CONTENT)];
+
+    // verifies that what was written is in the file.
+    assert(tfs_read(fhandle, buffer, sizeof(FILE_CONTENT)) ==
+           sizeof(FILE_CONTENT));
+    assert(memcmp(buffer, FILE_CONTENT, sizeof(FILE_CONTENT)) == 0);
+
+    assert(tfs_close(fhandle) == 0);
+
+    // confirms that none of these files exist
+    assert(tfs_open(SYM_LINK_1, TFS_O_TRUNC) == -1);
+    assert(tfs_open(SYM_LINK_2, TFS_O_TRUNC) == -1);
+
+    // creates a symbolic_link to the file
+    assert(tfs_sym_link(FILE_NAME, SYM_LINK_1) == 0);
+
+    // creates a symbolic link to the other symbolic link
+    assert(tfs_sym_link(SYM_LINK_1, SYM_LINK_2) == 0);
+
+    char buffer_sym_1[sizeof(FILE_CONTENT)];
+    char buffer_sym_2[sizeof(FILE_CONTENT)];
+
+    // confirms that the file content can be reached from both sym links
+    fhandle = tfs_open(SYM_LINK_1, TFS_O_CREAT);
+    assert(fhandle != -1);
+
+    assert(tfs_read(fhandle, buffer_sym_1, sizeof(FILE_CONTENT)) ==
+           sizeof(FILE_CONTENT));
+    assert(memcmp(buffer_sym_1, FILE_CONTENT, sizeof(FILE_CONTENT)) == 0);
+
+    assert(tfs_close(fhandle) == 0);
+
+    fhandle = tfs_open(SYM_LINK_2, TFS_O_CREAT);
+    assert(fhandle != -1);
+
+    assert(tfs_read(fhandle, buffer_sym_2, sizeof(FILE_CONTENT)) ==
+           sizeof(FILE_CONTENT));
+    assert(memcmp(buffer_sym_2, FILE_CONTENT, sizeof(FILE_CONTENT)) == 0);
+
+    assert(tfs_close(fhandle) == 0);
+
+    // now we'll unlink the file and try to make a circular link
+    assert(tfs_unlink(FILE_NAME) == 0);
+
+    // it should'nt add
+    assert(tfs_sym_link(SYM_LINK_2, FILE_NAME) == -1);
 
     // destroy TécnicoFS
     assert(tfs_destroy() != -1);
